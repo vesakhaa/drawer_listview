8f88432d7277a902e0403ef2b2eef2568ad18ece
vesakhaa <anandavesakha2905@gmail.com>
2024-11-09 08:34:48 +0700

    Second Commit

--- lib/drawer.dart
+++ lib/drawer.dart
@@ -18,30 +18,33 @@ class MyHomePage extends StatelessWidget{
               
       ),
       drawer: buildGroupDrawer(context),
-      body: GridView.count(
-      crossAxisCount: 2, 
-      crossAxisSpacing: 10.0,
-      mainAxisSpacing: 10.0,
-      shrinkWrap: true,
-      children: 
-        List.generate(10, (index){
-          return Padding(padding : EdgeInsets.all(10),
-          child: Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(20.0),
-              color: Colors.blue[200],
-            ),
-            child: Text ("Materi $index",
-            textAlign: TextAlign.center,
-            style : const TextStyle(
-              fontSize: 20.0,
-              color: Colors.black,
-            )
-            )
-            )
-          );
-        })
-      ),
+      body: Center(
+        child: Text("Pemrograman Mobile $title"),
+      )
+      // body: GridView.count(
+      // crossAxisCount: 2, 
+      // crossAxisSpacing: 10.0,
+      // mainAxisSpacing: 10.0,
+      // shrinkWrap: true,
+      // children: 
+      //   List.generate(10, (index){
+      //     return Padding(padding : EdgeInsets.all(10),
+      //     child: Container(
+      //       decoration: BoxDecoration(
+      //         borderRadius: BorderRadius.circular(20.0),
+      //         color: Colors.blue[200],
+      //       ),
+      //       child: Text ("Materi $index",
+      //       textAlign: TextAlign.center,
+      //       style : const TextStyle(
+      //         fontSize: 20.0,
+      //         color: Colors.black,
+      //       )
+      //       )
+      //       )
+      //     );
+      //   })
+      // ),
     );
   }
   Widget buildGroupDrawer(BuildContext context)  {
@@ -51,29 +54,53 @@ class MyHomePage extends StatelessWidget{
         children: [
           DrawerHeader(
             decoration: BoxDecoration(color: Colors.blue[900]),
-            child: const Text("Ananda Vesakhagotama Suparman",
-            style: TextStyle(color: Colors.white,
-            fontSize: 30.0),
+            child: UserAccountsDrawerHeader(
+              decoration: BoxDecoration(color: Colors.blue[900]),
+              accountName: Text("Ananda Vesakhagotama Suparman",
+              style: TextStyle(
+                fontSize: 18.0,
+              ),
+              ),
+              accountEmail: Text("anandavesakha2905@gmail.com",),
+              currentAccountPictureSize: Size.square(40.0),
+              currentAccountPicture: CircleAvatar(backgroundColor: Colors.white,
+              child: Text("V",
+              style: TextStyle(
+                fontSize: 25.0,
+                color: Colors.blue,
+              ),),),
             ),
-          ),
-          ExpansionTile (title: const Text("Materi"),
-          children: [
-            ListTile(title: const Text("Materi 1"),
-            onTap: (){Navigator.pop(context);},
-            ),
-            ListTile(title: Text("Materi 2"),
-            onTap: (){Navigator.pop(context);},
-            ),
-            ListTile(title: Text("Materi 3"),
-            onTap: (){Navigator.pop(context);},
-            ),
-            ListTile(title: Text("Materi 4"),onTap: (){Navigator.pop(context);},
-            ),
-            ListTile(title: Text("Materi 5"),
-            onTap: (){Navigator.pop(context);},
-            ),
-          ],
-          )
+          //   child: const Text("Ananda Vesakhagotama Suparman",
+          //   style: TextStyle(color: Colors.white,
+          //   fontSize: 30.0),
+          //   ),
+          // ),
+          // ExpansionTile (title: const Text("Materi"),
+          // children: [
+          //   ListTile(title: const Text("Materi 1"),
+          //   onTap: (){Navigator.pop(context);},
+          //   ),
+          //   ListTile(title: Text("Materi 2"),
+          //   onTap: (){Navigator.pop(context);},
+          //   ),
+          //   ListTile(title: Text("Materi 3"),
+          //   onTap: (){Navigator.pop(context);},
+          //   ),
+          //   ListTile(title: Text("Materi 4"),
+          //   onTap: (){Navigator.pop(context);},
+          //   ),
+          //   ListTile(title: Text("Materi 5"),
+          //   onTap: (){Navigator.pop(context);},
+          //   ),
+          // ],
+          // )
+      ),
+      ListTile(leading: Icon(Icons.person),
+      title: Text("Vesakhagotama"),
+      subtitle: Text("XII RPL 2"),
+      trailing: Icon(Icons.chevron_right),
+      ),
+
         ],
       ),
     );
